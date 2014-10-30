7c47501f60f744fd1c01825b013b93474520dabb
Folke Will <fwi@informatik.uni-kiel.de>
2014-10-30 14:56:02 +0100

    libuosl: Add methods to construct single and double click packets

--- libuosl/src/org/solhost/folko/uosl/libuosl/network/packets/SingleClickPacket.java
+++ libuosl/src/org/solhost/folko/uosl/libuosl/network/packets/SingleClickPacket.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright (c) 2013 Folke Will <folke.will@gmail.com>
- * 
+ *
  * This file is part of JPhex.
- * 
+ *
  * JPhex is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * JPhex is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -23,6 +23,14 @@ import java.nio.ByteBuffer;
 public class SingleClickPacket extends SLPacket {
     public static final short ID = 0x11;
     private long serial;
+
+    private SingleClickPacket() {
+    }
+
+    public SingleClickPacket(long serial) {
+        initWrite(ID, 8);
+        addUDWord(serial);
+    }
 
     public static SingleClickPacket read(ByteBuffer b, int len) {
         SingleClickPacket res = new SingleClickPacket();
